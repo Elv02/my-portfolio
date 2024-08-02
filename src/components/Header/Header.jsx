a8b95b8d0afe93faaf3fc6d98d33e7ba81b8b15f
Lauren 'Elv02' Hoeft <elv02x@gmail.com>
2024-08-02 16:20:48 -0400

    Added hamburger menu for header in mobile view.

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,28 +1,39 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import ThemeToggle from "../ThemeToggle/ThemeToggle";
-import "./Header.scss";
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import ThemeToggle from '../ThemeToggle/ThemeToggle';
+import './Header.scss';
 
 const Header = ({ onThemeChange }) => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
   return (
     <header className="header">
-      <nav>
-        <ul className="nav-list">
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/about">About</Link>
-          </li>
-          <li>
-            <Link to="/projects">Projects</Link>
-          </li>
-          <li>
-            <Link to="/contact">Contact</Link>
-          </li>
-        </ul>
-      </nav>
-      <ThemeToggle onThemeChange={onThemeChange} />
+      <div className="header__container">
+        <button className="header__menu-button" onClick={toggleMenu}>
+          &#9776;
+        </button>
+        <nav className={`header__nav ${isMenuOpen ? 'header__nav--open' : ''}`}>
+          <ul className="header__nav-list">
+            <li className="header__nav-item">
+              <Link to="/" onClick={toggleMenu}>Home</Link>
+            </li>
+            <li className="header__nav-item">
+              <Link to="/about" onClick={toggleMenu}>About</Link>
+            </li>
+            <li className="header__nav-item">
+              <Link to="/projects" onClick={toggleMenu}>Projects</Link>
+            </li>
+            <li className="header__nav-item">
+              <Link to="/contact" onClick={toggleMenu}>Contact</Link>
+            </li>
+          </ul>
+        </nav>
+        <ThemeToggle onThemeChange={onThemeChange} />
+      </div>
     </header>
   );
 };
