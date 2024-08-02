7d222abf65a08b6bcaa5ae1c88b0fbc8620b6435
Lauren 'Elv02' Hoeft <elv02x@gmail.com>
2024-08-02 15:07:14 -0400

    Added basic landing/home page and fixed rendering of the theme toggle in mobile views.

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,13 +1,31 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "./Home.scss";
+import headshotImage from "../../assets/images/Headshot.webp";
 
-function Home() {
+const Home = () => {
   return (
-    <div>
-      <h2>Home</h2>
-      <p>Welcome to my portfolio website</p>
+    <div className="home">
+      <div className="home__hero">
+        <img src={headshotImage} alt="Lauren Hoeft" className="home__hero__headshot" />
+        <h1 className="home__hero__title">Welcome to My Portfolio</h1>
+        <p className="home__hero__description">
+          Hi, I'm Lauren. I'm a passionate software developer with a love for creating innovative solutions.
+        </p>
+        <div className="home__hero__cta-buttons">
+          <Link to="/about" className="home__hero__cta-button">
+            About Me
+          </Link>
+          <Link to="/projects" className="home__hero__cta-button">
+            Projects
+          </Link>
+          <Link to="/contact" className="home__hero__cta-button">
+            Contact
+          </Link>
+        </div>
+      </div>
     </div>
   );
-}
+};
 
 export default Home;
