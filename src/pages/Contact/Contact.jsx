e8130311cdc5d15908fecd0ab29a22cb14048124
Lauren Hoeft <laurenhoeft42@gmail.com>
2024-08-06 00:44:52 -0400

    Updated contact page with contact links and updated formatting of footer.

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -1,11 +1,14 @@
 import React, { useState } from "react";
 import emailjs from "emailjs-com";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
+import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
 import "./Contact.scss";
 
 const Contact = () => {
   const [formData, setFormData] = useState({
-    name: "",
-    email: "",
+    user_name: "",
+    user_email: "",
     message: "",
   });
 
@@ -25,20 +28,20 @@ const Contact = () => {
         "service_vngsfxb", // Replace with your EmailJS service ID
         "template_ojge3we", // Replace with your EmailJS template ID
         e.target,
-        "-rFgg2370rbs0GLkX" // Replace with your EmailJS user ID
+        "-rFgg2370rbs0GLkX" // Replace with your EmailJS public key
       )
       .then(
-        (result) => {
+        (_result) => {
           alert("Message sent successfully!");
         },
-        (error) => {
+        (_error) => {
           alert("Failed to send the message, please try again.");
         }
       );
 
     setFormData({
-      name: "",
-      email: "",
+      user_name: "",
+      user_email: "",
       message: "",
     });
   };
@@ -47,25 +50,50 @@ const Contact = () => {
     <div className="contact">
       <div className="contact__container">
         <h1>Contact Me</h1>
+        <p>
+          You can reach me via the form below or through the following
+          platforms:
+        </p>
+        <div className="contact__icons">
+          <a
+            href="https://github.com/Elv02"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="contact__icon"
+          >
+            <FontAwesomeIcon icon={faGithub} />
+          </a>
+          <a
+            href="https://www.linkedin.com/in/lauren-hoeft"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="contact__icon"
+          >
+            <FontAwesomeIcon icon={faLinkedin} />
+          </a>
+          <a href="mailto:laurenhoeft42@gmail.com" className="contact__icon">
+            <FontAwesomeIcon icon={faEnvelope} />
+          </a>
+        </div>
         <form onSubmit={handleSubmit} className="contact__form">
           <div className="contact__form-group">
-            <label htmlFor="name">Name</label>
+            <label htmlFor="user_name">Name</label>
             <input
               type="text"
-              id="name"
-              name="name"
-              value={formData.name}
+              id="user_name"
+              name="user_name"
+              value={formData.user_name}
               onChange={handleChange}
               required
             />
           </div>
           <div className="contact__form-group">
-            <label htmlFor="email">Email</label>
+            <label htmlFor="user_email">Email</label>
             <input
               type="email"
-              id="email"
-              name="email"
-              value={formData.email}
+              id="user_email"
+              name="user_email"
+              value={formData.user_email}
               onChange={handleChange}
               required
             />
