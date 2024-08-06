c0b1e5ca537cbb0fb2597129f9e72b2cfb5eb0b1
Lauren Hoeft <laurenhoeft42@gmail.com>
2024-08-06 01:17:54 -0400

    Cleaned up formatting on home page, footer, and about cards. Prepping content for project page.

--- src/pages/Projects/Projects.jsx
+++ src/pages/Projects/Projects.jsx
@@ -1,11 +1,24 @@
 import React from "react";
 import "./Projects.scss";
+import Card from "/src/components/Card/Card";
 
 function Projects() {
   return (
     <div>
       <h2>Projects</h2>
-      <p>Check out some of my projects.</p>
+      <Card>
+        <p>Game programming project...</p>
+        <a>Github</a>
+      </Card>
+      <Card>
+        <p>Raycast javascript project...</p>
+      </Card>
+      <Card>
+        <p>TK Image Viewer</p>
+      </Card>
+      <Card>
+        <p>TK Calculator</p>
+      </Card>
     </div>
   );
 }
